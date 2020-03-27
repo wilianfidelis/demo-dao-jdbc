5e581628e20a47f8d335a48f77eca05bd2fa315d
wilianfidelis <wilian.fidelis.adler@gmail.com>
2020-03-27 14:34:25 -0300

    FinAll

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.Date;
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.DepartamentDao;
@@ -17,6 +18,19 @@ public class Program {
 		System.out.println("=== Test 1: seller findById ====");
 		Seller seller = sellerDao.findById(2);
 		System.out.println(seller);
+		
+		System.out.println("\n=== Test 1: seller findById ====");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+		
+		System.out.println("\n=== Test 1: seller findById ====");
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 		
 	}
 }
